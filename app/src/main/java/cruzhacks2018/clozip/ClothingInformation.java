73bb9acffeda68a13f2de9eff6d2008bf7c25a6d
Chris Mark <chris24mark@gmail.com>
2018-01-20 22:00:40 -0800

    simple ui for app pages

---
+++ app/src/main/java/cruzhacks2018/clozip/ClothingInformation.java
@@ -0,0 +1,13 @@
+package cruzhacks2018.clozip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClothingInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing_information);
+    }
+}
