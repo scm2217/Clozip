cca3f4e618e0d92f2036d722885e61613f451ba0
Sean Mendonca <smendonc@gmail.com>
2018-01-20 19:15:29 -0800

    added more pages

---
+++ app/src/main/java/cruzhacks2018/clozip/ChooseOutfit.java
@@ -0,0 +1,13 @@
+package cruzhacks2018.clozip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseOutfit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_outfit);
+    }
+}
--- app/src/main/java/cruzhacks2018/clozip/ClozipMenu.java
+++ app/src/main/java/cruzhacks2018/clozip/ClozipMenu.java
@@ -1,5 +1,6 @@
 package cruzhacks2018.clozip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,9 @@ public class ClozipMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        if (id == R.id.nav_newOutfit) {
+            startActivity(new Intent(ClozipMenu.this, ChooseOutfit.class));
+        } else if (id == R.id.nav_existingOutfits) {
 
         }
 
